218427555065145acb45aa2dca2c7766bd700f27
baohx <bao.hong.xin@gmail.com>
2026-06-15 13:54:30 +0800

    去除书籍的内容

--- book2/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Components Module
- * 
- * 导出所有 UI 组件
- */
-
-export { Sidebar, createSidebar, type SidebarOptions } from './sidebar';
-export { ProgressTracker, createProgressTracker } from './progress';
-export { Controls, createControls, type ControlsCallbacks } from './controls';
--- book2/src/parser/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Parser Module
- * 
- * 导出 Markdown 解析器和代码提取器
- */
-
-export { MarkdownParser } from './markdown';
-export { CodeExtractor } from './code-extractor';
